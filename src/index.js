3f7803ee49a3ea27407802842c3c77f10acac202
Negar Arabzadeh <negara@berkeley.edu>
2026-04-27 09:02:02 -0700

    Remove Worker config - switching to GitHub Pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// Worker entry point: delegate every request to the static assets binding.
-// All HTML/CSS/images live in the repo root and are served as-is.
-export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
-  },
-};
